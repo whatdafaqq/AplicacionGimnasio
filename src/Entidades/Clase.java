83fe50bee6f4dbcfba7916a8d40fbae65307c776
whatdafaqq <ramiromoran667@gmail.com>
2024-06-02 22:55:15 -0300

    Update Clase.java

--- src/Entidades/Clase.java
+++ src/Entidades/Clase.java
@@ -4,10 +4,76 @@
  */
 package Entidades;
 
+import java.time.LocalTime;
+
 /**
  *
  * @author ramir
  */
 public class Clase {
+    private int idClase;
+    private char nombre;
+    private int idEntrenador;
+    private LocalTime horario;
+    private int capacidad;
+    private boolean estado;
+
+    public Clase(int idClase, char nombre, int idEntrenador, LocalTime horario, int capacidad, boolean estado) {
+        this.idClase = idClase;
+        this.nombre = nombre;
+        this.idEntrenador = idEntrenador;
+        this.horario = horario;
+        this.capacidad = capacidad;
+        this.estado = estado;
+    }
+
+    public int getIdClase() {
+        return idClase;
+    }
+
+    public void setIdClase(int idClase) {
+        this.idClase = idClase;
+    }
+
+    public char getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(char nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getIdEntrenador() {
+        return idEntrenador;
+    }
+
+    public void setIdEntrenador(int idEntrenador) {
+        this.idEntrenador = idEntrenador;
+    }
+
+    public LocalTime getHorario() {
+        return horario;
+    }
+
+    public void setHorario(LocalTime horario) {
+        this.horario = horario;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+    
     
 }
